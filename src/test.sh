bba1ec2b85e2447ddbc46f3bac1e336949f1860e
Omar Fathy <omarfathy249202@gmail.com>
2025-12-11 13:47:57 +0200

    fix it

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./app.js)('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app.js)('Test'))")
 
 if ["$OUTPUT" == "$EXPECTED"]; then
 
